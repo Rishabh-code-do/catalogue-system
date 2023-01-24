0201fe5af513508e03c5336e5d6d27d0ed631760
Rishabh-code-do <rishabhjais01234@gmail.com>
2023-01-24 23:28:36 +0530

    Catalogue System

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Catalogue from './Catalogue';
+import Header from  './Header'
 
 function App() {
+  const [data, setData] = useState('');
+  
+  const handleData = (newData) => {
+    setData(newData);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header onDataUpdate={handleData}/>
+      <h1 className='text1'>{data}</h1>
+      <Catalogue category={data}/>
     </div>
   );
 }
